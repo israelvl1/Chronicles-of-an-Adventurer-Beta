5f0cf05eea5c7db9c3becb5cf0eec325e6c73241
israelvl1 <157428088+israelvl1@users.noreply.github.com>
2025-09-08 10:29:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(
+    os.path.dirname(os.path.abspath(__file__))
+) 
 import json
 from PyQt6.QtWidgets import QApplication
 from Jogo import *
